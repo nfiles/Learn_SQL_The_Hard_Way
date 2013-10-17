85b3fb3e88095c77815762a4230c5fea2a7b284f
Nathan Files <nfiles2009@gmail.com>
2013-10-17 16:30:27 -0400

    Setup - sublime project - testing script - ignore generated databases

---
+++ exec-sql.sh
@@ -0,0 +1,33 @@
+#! /bin/sh
+# automate testing sql
+# 
+# usage:
+# > sh exec-sql.sh [FILE]
+# where FILE is an SQL file within the 'exercises' directory
+
+NAME="exec-sql"
+PRE="[${NAME}]: "
+DB="sql-hard.db"
+DIR="exercises"
+
+# verify at least one argument
+if [ "$#" -ge 1 ]; then
+
+	# remove database if it already exists
+	if [ -f "${DB}" ]; then
+		rm "${DB}"
+	fi
+
+	# exercise file to be tested
+	FILE="${DIR}/$1"
+	if [ -f "${FILE}" ]; then
+		sqlite3 "${DB}" < "${FILE}"
+	else
+		echo "${PRE} \"${FILE}\" does not exist..."
+	fi
+
+# no exercise file
+else
+	echo "${PRE} Must supply an exercise file..."
+fi
+
