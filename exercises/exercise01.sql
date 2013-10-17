4834153f3cc036af8b7140986ee4ae50591e6739
Nathan Files <nfiles2009@gmail.com>
2013-10-17 16:31:06 -0400

    Exercise 1

--- exercises/exercise01.sql
+++ exercises/exercise01.sql
@@ -1,3 +1,9 @@
 -- Exercise 1: Creating Tables
 -- http://sql.learncodethehardway.org/book/ex1.html
 
+CREATE TABLE person (
+	id  	   INTEGER PRIMARY KEY,
+	first_name TEXT,
+	last_name  Text,
+	age        INTEGER
+);
