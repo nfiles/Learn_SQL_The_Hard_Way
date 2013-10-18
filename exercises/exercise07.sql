92fb0035bd3cd6b883880016e37ebf53a013d798
Nathan Files <nfiles2009@gmail.com>
2013-10-18 11:02:10 -0400

    Exercise 7

---
+++ exercises/exercise07.sql
@@ -0,0 +1,61 @@
+-- Exercise 2: Creating a Multi-Table Database
+-- http://sql.learncodethehardway.org/book/ex3.html
+
+CREATE TABLE person (
+	id         INTEGER PRIMARY KEY,
+	first_name TEXT,
+	last_name  TEXT,
+	age        INTEGER
+);
+
+CREATE TABLE pet (
+	id          INTEGER PRIMARY KEY,
+	name        TEXT,
+	breed       TEXT,
+	age         INTEGER,
+	dead        INTEGER,
+	disposition TEXT
+);
+
+CREATE TABLE person_pet (
+	person_id INTEGER,
+	pet_id    INTEGER
+);
+
+
+-- Exercise 3: Inserting Data
+-- http://sql.learncodethehardway.org/book/ex3.html
+
+INSERT INTO person (id, first_name, last_name, age)
+	VALUES (0, "Priscilla", "Files", 22);
+
+INSERT INTO pet (id, name, breed, age, dead, disposition)
+	VALUES (0, "Beans", "rabbit", 4, 0, "Hungry, but social");
+
+INSERT INTO pet (id, name, breed, age, dead, disposition)
+	VALUES (1, "Foofs", "rabbit", 10, 1, "Cute. Puts up with Josiah. :)");
+
+
+-- Exercise 4: Insert Referential Data
+-- http://sql.learncodethehardway.org/book/ex4.html
+
+INSERT INTO person_pet (person_id, pet_id)
+	VALUES (0, 0);
+
+INSERT INTO person_pet (person_id, pet_id)
+	VALUES (0, 1);
+
+
+-- Exercise 7: Deleting Data
+-- http://sql.learncodethehardway.org/book/ex7.html
+
+SELECT name, age FROM pet WHERE dead = 1;
+
+DELETE FROM pet WHERE dead = 1;
+
+SELECT * FROM pet;
+
+INSERT INTO pet (id, name, breed, age, dead, disposition)
+	VALUES (1, "Foofs", "rabbit", 10, 1, "Cute. Puts up with Josiah. :)");
+
+SELECT * FROM pet;
